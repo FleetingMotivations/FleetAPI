19293bf1b97245920f8b13f6e68459a2e9bbd29b
Tristan Newmann <tnew2294@gmail.com>
2016-10-04 17:29:32 +1100

    Update SP commit

--- FleetApi/Controllers/HomeController.cs
+++ FleetApi/Controllers/HomeController.cs
@@ -28,11 +28,48 @@ namespace FleetApi.Controllers
                 var campuses = db.Campuses
                     .Select(c => new
                     {
+                        Id = c.CampusId,
                         CampusName = c.CampusIdentifer
                     })
                     .ToList();
 
                 return Ok(campuses);
+            }
+        }
+
+        [Route("buildings")]
+        public IHttpActionResult GetBuildings(int campusId)
+        {
+            using (var db = new FleetContext())
+            {
+                var buildings = db.Buildings
+                    .Where(b => b.CampusId == campusId)
+                    .Select(c => new
+                    {
+                        Id = c.BuildingId,
+                        CampusName = c.BuildingIdentifier
+                    })
+                    .ToList();
+
+                return Ok(buildings);
+            }
+        }
+            
+        [Route("rooms")]
+        public IHttpActionResult GetRooms(int buildingId)
+        {
+            using (var db = new FleetContext())
+            {
+                var rooms = db.Rooms
+                    .Where(r => r.BuildingId == buildingId)
+                    .Select(r => new
+                    {
+                        Id = r.RoomId,
+                        RoomName = r.RoomIdentifier
+                    })
+                    .ToList();
+
+                return Ok(rooms);
             }
         }
     }
